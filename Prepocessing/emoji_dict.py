a4b3c9b780161ac28bcce520529cf5803fb6582f
Emanuel Embuai <emanuelembuai99@gmail.com>
2026-08-24 12:44:02 +0900

    feat: upload full thesis project files

---
+++ Prepocessing/emoji_dict.py
@@ -0,0 +1,57 @@
+#Konversi emoticon menjadi kata
+emoji_dict = {
+    # sedih / duka / prihatin
+    "😭": " sedih ",
+    "😢": " sedih ",
+    "🥲": " sedih ",
+    "🥺": " sedih ",
+    "😔": " sedih ",
+    "😞": " sedih ",
+    "💔": " sedih ",
+    "🥀": " sedih ",
+    "❤️‍🩹": " pulih ",
+
+    # doa / harapan / empati
+    "🙏": " doa ",
+    "🙏🏻": " doa ",
+    "🤲": " doa ",
+    "🥹": " haru ",
+
+    # dukungan / apresiasi / semangat
+    "💪": " semangat ",
+    "👍": " setuju ",
+    "✅": " setuju ",
+    "🤝": " kerja sama ",
+    "💖": " dukungan ",
+    "❤️": " dukungan ",
+    "❤": " dukungan ",
+    "🤍": " dukungan ",
+    "💚": " dukungan ",
+
+    # positif / senang
+    "😊": " senang ",
+    "☺️": " senang ",
+    "🥰": " senang ",
+
+    # peringatan / darurat
+    "🚨": " darurat ",
+    "‼️": " peringatan ",
+    "📢": " informasi ",
+    "📍": " lokasi ",
+    "🗓": " waktu ",
+    "🌧️": " hujan ",
+    "🌀": " badai ",
+
+    # bingung / tanya / kritik
+    "🤔": " bingung ",
+    "🙃": " bingung ",
+
+    # tertawa
+    "🤣": " tertawa ",
+
+    # lainnya, jika tidak terlalu relevan
+    "🎥": " video ",
+    "👶": " bayi ",
+    "🤱": " ibu bayi ",
+    "🇮🇩": " indonesia "
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+"# Thesis-LSTM-IndoBERT" 
